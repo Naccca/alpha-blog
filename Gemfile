40938cb30c85d86922610f956fc4c89ec7ad3d1f
Nadezda Jeremic <nadezdajeremic@hotmail.com>
2019-04-14 21:47:22 +0200

    Style fixes. Rubocop enabled

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
-#gem 'mini_racer', platforms: :ruby
+# gem 'mini_racer', platforms: :ruby
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
@@ -43,13 +43,13 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   gem 'sqlite3'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
 end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'web-console', '>= 3.3.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
@@ -68,6 +68,9 @@ group :production do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
-#gem 'bootstrap', '~> 4.3.1'
+# gem 'bootstrap', '~> 4.3.1'
+
+# BeautifyRuby - HTML - https://github.com/threedaymonk/htmlbeautifier
+gem 'htmlbeautifier'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,54 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update, :show]
-	before_action :require_same_user, only: [:edit, :update]
-	def index
-		@users = User.paginate(page: params[:page], per_page: 5)
-	end
+  before_action :set_user, only: %i[edit update show]
+  before_action :require_same_user, only: %i[edit update]
 
-	def new
-		@user = User.new
-	end
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			flash[:success] = "Welcome to the Alpha blog #{@user.username}"
-			redirect_to articles_path
-		else
-			render 'new'
-		end
-	end
+  def new
+    @user = User.new
+  end
 
-	def edit
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Alpha blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
 
-	def update
-		if @user.update(user_params)
-			flash[:success] = "Your account was updated successfully"
-			redirect_to articles_path
-		else
-			render 'edit'
-		end
-	end
+  def edit; end
 
-	def show
-		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-	end
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'Your account was updated successfully'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
 
-	private
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
 
-	def user_params
-		params.require(:user).permit(:username, :email, :password)
-	end
+  private
 
-	def set_user
-		@user = User.find(params[:id])
-	end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
-	def require_same_user
-		if current_user != @user
-			flash[:danger] = "You can not edit this account"
-			redirect_to root_path
-		end
-	end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
+  def require_same_user
+    return if current_user == @user
+
+    flash[:danger] = 'You can not edit this account'
+    redirect_to root_path
+  end
 end
